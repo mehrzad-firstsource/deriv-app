59d3401153cfdad1f626c9181c0e7c6afca968ee
mehrzad-fs <mehrzad@firstsource.tech>
2020-05-04 12:25:44 +0430

    initial react virtualized

--- packages/bot-web-ui/src/components/transactions.jsx
+++ packages/bot-web-ui/src/components/transactions.jsx
@@ -1,7 +1,8 @@
 import { ThemedScrollbars, Icon } from '@deriv/components';
 import { localize } from '@deriv/translations';
-import { PropTypes } from 'prop-types';
+import { PropTypes, element } from 'prop-types';
 import React from 'react';
+import { List, AutoSizer } from 'react-virtualized';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Transaction from './transaction.jsx';
 import { transaction_elements } from '../constants/transactions';
@@ -17,7 +18,42 @@ class Transactions extends React.PureComponent {
     componentWillUnmount() {
         this.props.onUnmount();
     }
-
+    renderRow = ({ index, style }) => {
+        const element = this.props.elements[index];
+        // return (
+        //     <div key={index} style={style}>
+        //         this is element {index}
+        //     </div>
+        // )
+        switch (element.type) {
+            case transaction_elements.CONTRACT: {
+                const { data: contract } = element;
+                const { buy } = contract.transaction_ids;
+                return (
+                    <CSSTransition key={index} timeout={500} classNames='list__animation'>
+                        <Transaction contract={contract} />
+                    </CSSTransition>
+                );
+            }
+            case transaction_elements.DIVIDER: {
+                const { data: run_id } = element;
+                return (
+                    <CSSTransition key={index} timeout={500}>
+                        <div key={run_id} className='transactions__divider'>
+                            <div className='transactions__divider-line' />
+                        </div>
+                    </CSSTransition>
+                );
+            }
+            default: {
+                return null;
+            }
+        }
+    };
+    handleScroll = ({ target }) => {
+        const { scrollTop, scrollLeft } = target;
+        this.list_ref.Grid.handleScrollEvent({ scrollTop, scrollLeft });
+    };
     render() {
         const { contract_stage, elements } = this.props;
         return (
@@ -31,65 +67,68 @@ class Transactions extends React.PureComponent {
                     </span>
                 </div>
                 <div className='transactions__content'>
-                    <ThemedScrollbars autoHide>
-                        {elements.length ? (
-                            <TransitionGroup>
-                                {elements.map(element => {
-                                    switch (element.type) {
-                                        case transaction_elements.CONTRACT: {
-                                            const { data: contract } = element;
-                                            const { buy } = contract.transaction_ids;
-                                            return (
-                                                <CSSTransition key={buy} timeout={500} classNames='list__animation'>
-                                                    <Transaction contract={contract} />
-                                                </CSSTransition>
-                                            );
-                                        }
-                                        case transaction_elements.DIVIDER: {
-                                            const { data: run_id } = element;
-                                            return (
-                                                <CSSTransition key={run_id} timeout={500}>
-                                                    <div key={run_id} className='transactions__divider'>
-                                                        <div className='transactions__divider-line' />
-                                                    </div>
-                                                </CSSTransition>
-                                            );
-                                        }
-                                        default: {
-                                            return null;
-                                        }
-                                    }
-                                })}
-                            </TransitionGroup>
-                        ) : (
-                            <>
-                                {contract_stage.index >= contract_stages.STARTING.index ? (
-                                    <Transaction contract={null} />
-                                ) : (
-                                    <div className='transactions-empty__container'>
-                                        <div className='transactions-empty'>
-                                            <Icon
-                                                icon='IcBox'
-                                                className='transactions-empty__icon'
-                                                size={64}
-                                                color='secondary'
-                                            />
-                                            <h4 className='transactions-empty__header'>
-                                                {localize('There are no transactions to display')}
-                                            </h4>
-                                            <div className='transactions-empty__message'>
-                                                <span>{localize('Here are the possible reasons:')}</span>
-                                                <ul className='transactions-empty__list'>
-                                                    <li>{localize('The bot is not running')}</li>
-                                                    <li>{localize('The stats are cleared')}</li>
-                                                </ul>
-                                            </div>
+                    {elements.length ? (
+                        <TransitionGroup>
+                            <AutoSizer style={{ height: 'calc(var(--drawer-scroll-height) - 7px)' }}>
+                                {({ width, height }) => (
+                                    <ThemedScrollbars
+                                        style={{
+                                            height,
+                                            width,
+                                        }}
+                                        onScroll={this.handleScroll}
+                                        autoHide
+                                    >
+                                        <List
+                                            containerStyle={{ marginBottom: 117 }}
+                                            style={{
+                                                overflowX: false,
+                                                overflowY: false,
+                                            }}
+                                            ref={ref => (this.list_ref = ref)}
+                                            className='transactions__windowing'
+                                            height={height}
+                                            rowCount={elements.length}
+                                            rowHeight={element =>
+                                                this.props.elements[element.index].type === transaction_elements.DIVIDER
+                                                    ? 20
+                                                    : 55
+                                            }
+                                            rowRenderer={this.renderRow}
+                                            width={width}
+                                        />
+                                    </ThemedScrollbars>
+                                )}
+                            </AutoSizer>
+                        </TransitionGroup>
+                    ) : (
+                        <>
+                            {contract_stage.index >= contract_stages.STARTING.index ? (
+                                <Transaction contract={null} />
+                            ) : (
+                                <div className='transactions-empty__container'>
+                                    <div className='transactions-empty'>
+                                        <Icon
+                                            icon='IcBox'
+                                            className='transactions-empty__icon'
+                                            size={64}
+                                            color='secondary'
+                                        />
+                                        <h4 className='transactions-empty__header'>
+                                            {localize('There are no transactions to display')}
+                                        </h4>
+                                        <div className='transactions-empty__message'>
+                                            <span>{localize('Here are the possible reasons:')}</span>
+                                            <ul className='transactions-empty__list'>
+                                                <li>{localize('The bot is not running')}</li>
+                                                <li>{localize('The stats are cleared')}</li>
+                                            </ul>
                                         </div>
                                     </div>
-                                )}
-                            </>
-                        )}
-                    </ThemedScrollbars>
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
         );
