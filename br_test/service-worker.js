8a3c45330284b753763fe480d6c4453dcbcc460b
mehrzad-fs <mehrzad@firstsource.tech>
2020-07-08 12:16:02 +0430

    Updates

--- br_test/service-worker.js
+++ br_test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_test/precache-manifest.333c68ef999e4118d5e95616c0a4cfb4.js"
+  "/br_test/precache-manifest.e588df72d0fac6d3ed1684914a021466.js"
 );
 
 workbox.core.skipWaiting();
